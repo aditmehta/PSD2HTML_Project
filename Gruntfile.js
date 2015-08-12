560319f790391a0be3cc6d6806fccfc7d6cdd73b
aditmehta <aditmehta10@gmail.com>
2015-08-12 15:29:43 +0530

    Minify and Concatenate

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,12 +12,27 @@ grunt.initConfig({
     watch: {
       css: {
         files: ['assets/stylesheets/src/*.scss'],
-        tasks: ['sass']
+        tasks: ['sass','cssmin']
       },
     },
+    //   concat: {
+    //   css: {
+    //     src: ['assets/stylesheets/src/common.css', 'assets/stylesheets/src/background.css'],
+    //     dest: 'assets/stylesheets/app.css',
+    //   },
+    // },
+      cssmin: {
+        target: {
+          files: {
+            'assets/stylesheets/app.min.css': ['assets/stylesheets/src/common.css', 'assets/stylesheets/src/background.css']
+          }
+        }
+      }
 });
 
 grunt.loadNpmTasks('grunt-contrib-sass');
 grunt.loadNpmTasks('grunt-contrib-watch');
-grunt.registerTask("default", ["sass","watch"]);
+// grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-cssmin'); 
+grunt.registerTask("default", ["sass","cssmin","watch"]);
 };
